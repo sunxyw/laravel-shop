7b5c13b06316d06a3c895d0c538e36bd60bfdb46
sunxyw <xy2496419818@gmail.com>
2019-02-03 14:16:59 +0800

    ai

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('addresses', 'UserAddressController@index')->name('user_addresses.index');
+    Route::get('addresses/create', 'UserAddressController@create')->name('user_addresses.create');
 });
